e1c35e63924c8f4a24a9826c814bc6adffb4d522
josh-white <joshua.jay.white@gmail.com>
2021-12-02 13:53:23 -0600

    initial push

---
+++ src/main/kotlin/com/battleshipworkshop/personal/PersonalApplication.kt
@@ -0,0 +1,11 @@
+package com.battleshipworkshop.personal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PersonalApplication
+
+fun main(args: Array<String>) {
+	runApplication<PersonalApplication>(*args)
+}
